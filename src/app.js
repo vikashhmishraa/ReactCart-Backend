0c0a93bedc1cce3a653336b6593b0887ea3f7b91
vikashhmishraa <vikashmishra749@gmail.com>
2024-10-01 12:26:22 +0530

    but fix

--- src/app.js
+++ src/app.js
@@ -2,13 +2,13 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 const app = express();
-
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
+    origin:'http://localhost:5173', // or specify the origin of your React app
+    credentials: true, // Enable this if you need to send cookies or authentication headers
   })
 );
+
 
 app.use(express.json());
 app.use(express.urlencoded());
